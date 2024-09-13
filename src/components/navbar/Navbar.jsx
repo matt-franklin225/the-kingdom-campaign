863a18a16fd2e9775794c8db4869c64ac738d217
Matt <137856149+matt-franklin225@users.noreply.github.com>
2024-09-12 19:44:40 -0700

    The Kingdom Campaign

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/tkc_logo.png'
 import './navbar.css';
 
 const Menu = () => (
     <>
-    <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">What is GPT?</a></p>
-    <p><a href="#possibility">OpenAI</a></p>
-    <p><a href="#features">Case Studies</a></p>
-    <p><a href="#blog">Library</a></p>
+    <p><a href="#plot">Plot</a></p>
+    <p><a href="#characters">Characters</a></p>
+    <p><a href="#npcs">NPCs</a></p>
+    <p><a href="#art">Art</a></p>
+    <p><a href="#map">Map</a></p>
     </>
 )
 
@@ -26,10 +26,10 @@ const Navbar = () => {
                     <Menu />
                 </div>
             </div>
-            <div className="gpt3__navbar-sign">
+            {/* <div className="gpt3__navbar-sign">
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
-            </div>
+            </div> */}
             <div className="gpt3__navbar-menu">
                 {toggleMenu
                     ? <RiCloseLine color="fff" size = {27} onClick={() => setToggleMenu(false)} />
@@ -39,10 +39,10 @@ const Navbar = () => {
                     <div className="gpt3__navbar-menu_container scale-up-center">
                         <div className="gpt3__navbar-menu_container-links">
                             <Menu />
-                            <div className=".gpt3__navbar-menu_container-links-sign">
+                            {/* <div className=".gpt3__navbar-menu_container-links-sign">
                                 <p>Sign in</p>
                                 <button type="button">Sign up</button>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 )}
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './header.css'
 import people from '../../assets/people.png'
-import ai from '../../assets/ai.png'
+import party_drawing from '../../assets/party_drawing.png'
 
 const Header = () => {
     return (
         <div className="gpt3__header section__padding" id="home">
             <div className="gpt3__header-content">
-                <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
-                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <h1 className="gradient__text">The Kingdom Campaign: From Waterdeep to Fireshallow</h1>
+                <p>The journey of a small band of misfits, beginning as humble adventurers but evolving into powerful rulers capable of taking on the world's mightiest foes.</p>
 
-                <div className="gpt3__header-content__input">
+                {/* <div className="gpt3__header-content__input">
                     <input type="email" placeholder="Your Email Address"></input>
                     <button type="button">Get Started</button>
                 </div>
@@ -18,11 +18,11 @@ const Header = () => {
                 <div className="gpt3__header-content__people">
                     <img src={people} alt="people" />
                     <p>1,600 people requested access a visit in last 24 hours</p>
-                </div>
+                </div> */}
             </div>
 
             <div className="gpt3__header-image">
-                <img src={ai} alt="ai"></img>
+                <img src={party_drawing} alt="Party Drawing"></img>
             </div>
             
         </div>
