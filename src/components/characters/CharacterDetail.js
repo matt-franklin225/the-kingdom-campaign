daf52914d9b03ee42acbcf5df56fb859fda6849f
Matt <137856149+matt-franklin225@users.noreply.github.com>
2024-09-15 20:41:39 -0700

    Individual Character Page 1

--- src/components/characters/CharacterDetail.js
+++ src/components/characters/CharacterDetail.js
@@ -3,6 +3,7 @@ import { Footer } from '../../containers';
 import { Break, Navbar } from '../';
 import { useParams } from 'react-router-dom';
 import { characters } from './data/characters';
+import './character_detail.css'
 
 function CharacterDetail() {
     // const { id } = useParams();
@@ -16,15 +17,67 @@ function CharacterDetail() {
 
     return (
         
-        <div className="character_detail section_margin">
+        <div className="tkc__character_detail section_margin">
             <div className="gradient__bg">
                 <Navbar />
-                <div className="character_detail-info section__padding">
-                    <div className="character_detail-info-heading">
-                        <h1>{character.name}</h1>
-                        <h2>{character.title}</h2>
+                <div className="tkc__character_detail-info section__padding">
+                    <div className="tkc__character_detail-info-heading">
+                        <div className="tkc__character_detail-name">
+                            <h1>{character.name}</h1>
+                        </div>
+                        <div className="tkc__character_detail-image">
+                            <img src={character.imgUrl} alt={character.name}/>
+                        </div>
+                        <div className="tkc__character_detail-title">
+                            <h2>{character.title}</h2>
+                        </div>
                     </div>
-                    
+                    <div className="tkc__character_detail-info-details">
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Class:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.class}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Race:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.race}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Alignment:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.alignment}</h3>
+                        </div>
+                    </div>
+                    <div className="tkc__character_detail-info-details">
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Age:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.age}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Birthplace:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.birthplace}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Other Title:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.otherTitle}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="tkc__character_detail-description">
+                    <h1>Description</h1>
+                    <div className="tkc__character_detail-description-text">
+                        <h3>{character.description}</h3>
+                    </div>
                 </div>
             </div>
             <Break />
