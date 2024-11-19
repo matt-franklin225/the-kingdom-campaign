f89f3fb8694aab13629de88ff2712a6ce9485f00
Matt Franklin <mjfrankl@uci.edu>
2024-11-18 20:19:10 -0800

    Fixed Characters, Beginning NPCs

---
+++ src/components/npc/NpcDetail.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Footer } from '../../containers';
+import { Break, Navbar } from '..';
+import { useParams } from 'react-router-dom';
+import { characters } from './data/npcs';
+import './character_detail.css'
+
+function CharacterDetail() {
+    const { loc } = useParams();
+    const character = characters.find((char) => char.loc === loc);
+
+    if (!character) {
+        return <h2>Character not found</h2>;
+    }
+
+    return (
+        
+        <div className="tkc__character_detail section_margin">
+            <div className="gradient__bg">
+                <Navbar />
+                <div className="tkc__character_detail-info section__padding">
+                    <div className="tkc__character_detail-info-heading">
+                        <div className="tkc__character_detail-name">
+                            <h1>{character.name}</h1>
+                        </div>
+                        <div className="tkc__character_detail-image">
+                            <img src={character.imgUrl} alt={character.name}/>
+                        </div>
+                        <div className="tkc__character_detail-title">
+                            <h2>{character.title}</h2>
+                        </div>
+                    </div>
+                    <div className="tkc__character_detail-info-details">
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Class:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.class}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Race:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.race}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Alignment:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.alignment}</h3>
+                        </div>
+                    </div>
+                    <div className="tkc__character_detail-info-details">
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Age:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.age}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Birthplace:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.birthplace}</h3>
+                        </div>
+                        <div className="tkc__character_detail-trait">
+                            <h2>{'Other Title:'}</h2>
+                        </div>
+                        <div className="tkc__character_detail-value">
+                            <h3>{character.otherTitle}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="tkc__character_detail-description">
+                    <h1>Description</h1>
+                    <div className="tkc__character_detail-description-text">
+                        <h3>{character.description}</h3>
+                    </div>
+                </div>
+                <div className="tkc__character_detail-description">
+                    <h1>Notable Accomplishments</h1>
+                    <div className="tkc__character_detail-description-text">
+                        <h3>{character.accomplishments}</h3>
+                    </div>
+                </div>
+            </div>
+            <Break />
+            <Footer />
+        </div>
+    );
+}
+
+export default CharacterDetail;
--- src/containers/characters/Characters.js
+++ src/containers/characters/Characters.js
@@ -23,11 +23,11 @@ function Characters() {
           <h1><a href="/characters">Characters</a></h1>
         </div>
         <div className="tkc__characters-container">
-          <div id="characters-list">
+        <div id="characters-list" className="characters-list">
             {characterEpisodes.length > 0 ? (
                 characterEpisodes.map((character) => (
-                    <div key={character.id} className="character">
-                        <Character name={character.name} imgUrl={character.imgUrl} text={character.title} page={character.loc}/>
+                    <div key={character.id} className="character" id="character">
+                        <Character name={character.name} imgUrl={character.imgUrl} text={character.title} page={`characters/${character.loc}`}/>
                     </div>
                 ))
             ) : (
@@ -36,15 +36,16 @@ function Characters() {
           </div>
         </div>
       </div>
-    );
 
-      {/* <Character name="Eugene Rider" imgUrl={eugene} text="High King of Fireshallow" page="/characters/eugene_rider"/>
-      <Character name="Pluto Johnson" imgUrl={pluto} text="Lord of the Golden Order" page="/characters/pluto_johnson"/>
-      <Character name="Ariah Rhialla" imgUrl={ariah} text="Architect of the EmberWild" page="/characters/ariah_rhialla"/>
-      <Character name="Opal Blade" imgUrl={opal} text="Brightcandle of Fireshallow" page="/characters/opal_blade"/>
-      <Character name="Sarenia Valen" imgUrl={sarenia} text="Exalted Singbird of Fireshallow" page="/characters/sarenia_valen"/>
-      <Character name="Berrían Berevan" imgUrl={berrian} text="Elder of Ashenveil Forest" page="/characters/berrian_berevan"/>
-      <Character name="Jewel Dragoneere" imgUrl={jewel} text="Avenger of the Dragonborn" page="/characters/jewel_dragoneere"/>
-      <Character name="Lēg Corrunpo" imgUrl={leg} text="Champion of Avandra" page="/characters/st_leg"/> */}
-}
+      // {/* <Character name="Eugene Rider" imgUrl={eugene} text="High King of Fireshallow" page="/characters/eugene_rider"/>
+      // <Character name="Pluto Johnson" imgUrl={pluto} text="Lord of the Golden Order" page="/characters/pluto_johnson"/>
+      // <Character name="Ariah Rhialla" imgUrl={ariah} text="Architect of the EmberWild" page="/characters/ariah_rhialla"/>
+      // <Character name="Opal Blade" imgUrl={opal} text="Brightcandle of Fireshallow" page="/characters/opal_blade"/>
+      // <Character name="Sarenia Valen" imgUrl={sarenia} text="Exalted Singbird of Fireshallow" page="/characters/sarenia_valen"/>
+      // <Character name="Berrían Berevan" imgUrl={berrian} text="Elder of Ashenveil Forest" page="/characters/berrian_berevan"/>
+      // <Character name="Jewel Dragoneere" imgUrl={jewel} text="Avenger of the Dragonborn" page="/characters/jewel_dragoneere"/>
+      // <Character name="Lēg Corrunpo" imgUrl={leg} text="Champion of Avandra" page="/characters/st_leg"/> */}
+    );
+  }
+
 export default Characters;
